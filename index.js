618117cf2abe43127eeb2b221096611b62588514
Regine26-Adam13 <72616516+Regine26-Adam13@users.noreply.github.com>
2020-10-09 23:02:18 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Sample JS</h2>
+
+<p id="demo"> Welcome To My Life. </p>
+
+<button type="button" onclick='document.getElementById("demo").innerHTML = "Ohayoooo!"'>Click Me!</button>
+
+</body>
+</html>
